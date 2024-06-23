bee0f5901e72e0484554da6ff85e2a8d9fe9792a
CletusAAbugre <c.abugre@alustudent.com>
2024-06-23 12:54:28 -0700

    Positive anything is better than negative nothing

--- 3-print_alphabt.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-for alphabet in range(ord('a'), ord('z') + 1):
-    if alphabet != ord('e') and alphabet != ord('q'):
-        print("{:c}".format(alphabet), end="")
--- 7-islower.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-def islower(ish):
-    if ord(ish) >= ord('a') and ord(ish) <= ord('z'):
-        return True
-    else:
-        return False
